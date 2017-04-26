e884398eb4a7bffb07bff8f338fd4352c688a3fb
Padraic O'Neill <pconeill@gmail.com>
2017-04-26 12:25:12 -0600

    background color added for high/low/correct guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,16 +6,19 @@ SECRET_NUMBER = rand(100)
 get '/' do
   guess = params['guess'].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => @@color}
 end
 
 def check_guess(guess)
   message = ""
-  if guess > SECRET_NUMBER
+  if guess > SECRET_NUMBER + 5 || guess < SECRET_NUMBER - 5
+    @@color = 'red'
     message = "Too high!"
   elsif guess < SECRET_NUMBER
+    @@color = '#FF9999'
     message = "Too low!"
   else
+    @@color = 'green'
     message = "You got it!"
   end
 end
